9dc17ba9cd840f25ea60bff4b9f2fb75c834c415
patriciaalves232 <patricia.rh23@outlook.com>
2022-06-09 20:00:45 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let nome = 'Patricia';
+let sexo = 'fem';
+if(!nome){
+ nome = 'Patricia Alves';
+ console.log(nome);
+console.log(sexo);
+}
+nome = 'thainara';
+console.log(nome);
+ console.log(sexo);
